e587107f1fd5a1b2c6aec0eff97d213bb00478ac
NellyBise <nellybise@free.fr>
2024-09-13 17:11:34 +0200

    Add Price section

--- app/components/PricingCards.js
+++ app/components/PricingCards.js
@@ -1,25 +1,68 @@
+'use client'
+
 import prices from '../data/prices.json'
+import { useState } from 'react'
 
 export default function PrincingCards() {
+  const [check, setCheck] = useState(false)
+
+  const handleCheckboxChange = () => {
+    setCheck(!check)
+  }
+
   return (
-    <section>
-      {prices.map((price) => (
-        <article
-          key={price.title}
-          className="p-4 text-left border-[1px] border-neutral-200 rounded-xl bg-gradient-to-tl from-neutral-100 max-w-[500px]"
+    <section className="flex flex-col items-center">
+      <div className="flex gap-2 items-center h-24 mb-14">
+        <p
+          className={`w-[500px] text-right ${
+            check ? 'text-xl' : 'font-bold text-5xl'
+          }`}
         >
-          <h3 className="text-2xl font-bold">{price.title}</h3>
-          <p className="text-base">{price.title}</p>
-          <p className="text-base">{price.price}</p>
-          <p className="text-base">{price.description}</p>
-          <div className="border-px border-black w-full" />
-          <ul className="flex gap-2 items-center">
-            {price.included.map((item) => (
-              <li key={`${price.title}-${item}`}>{item}</li>
-            ))}
-          </ul>
-        </article>
-      ))}
+          Billed Monthly
+        </p>
+        <label class="inline-flex items-center cursor-pointer">
+          <input
+            type="checkbox"
+            value=""
+            class="sr-only peer"
+            checked={check}
+            onClick={handleCheckboxChange}
+          />
+          <div class="relative w-20 h-11 bg-amber-400 peer-focus:outline-none peer-focus:ring-1 ring-grey rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[4px]  after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-9  after:w-9 after:transition-all dark:border-gray-600"></div>
+        </label>
+        <p className={`w-[500px] ${check ? 'font-bold text-5xl' : 'text-xl'}`}>
+          Billed Yearly <span className="text-xl items-center">(save 15%)</span>
+        </p>
+      </div>
+      <div className="flex gap-6 text-left text-xl">
+        {prices.map((price) => (
+          <div
+            key={price.title}
+            className={`p-8 text-left border-2  rounded-xl bg-gradient-to-tl from-neutral-100 max-w-[500px] h-max ${
+              price.title === 'Pro' ? 'border-amber-400' : 'border-neutral-200'
+            }`}
+          >
+            <h3 className="text-[34px] font-bold">{price.title}</h3>
+            <p className="text-[38px] my-8 font-bold">
+              {check ? price.yearly : price.monthly}
+              <span>{check ? '/ year' : '/ month'} </span>
+              <span className="line-through">
+                {check && price.title === 'Pro' ? '($144)' : ''}
+              </span>
+            </p>
+            <p className="">{price.description}</p>
+            <div className="border-[1px] border-grey w-full my-12" />
+            <ul className="flex flex-col gap-4 list-disc list-inside">
+              {price.included.map((item) => (
+                <li key={`${price.title}-${item}`}>{item}</li>
+              ))}
+            </ul>
+            <button className="w-full py-4 mt-8 bg-amber-400 rounded-full text-white hover:bg-amber-500 ease-in-out duration-300">
+              {price.button}
+            </button>
+          </div>
+        ))}
+      </div>
     </section>
   )
 }
